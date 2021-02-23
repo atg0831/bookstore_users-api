99cef562b0b66588ed1eb0f2f51d1711c5f46b95
atg0831 <atg0831@naver.com>
2021-02-23 18:36:32 +0900

    Feat: make utils and use it

---
+++ utils/date/date_utils.go
@@ -0,0 +1,15 @@
+package date
+
+import "time"
+
+const (
+	apiCurrentDate = "2006-01-02T15:04:05Z"
+)
+
+func GetNow() time.Time {
+	return time.Now()
+}
+
+func GetNowString() string {
+	return GetNow().Format(apiCurrentDate)
+}
